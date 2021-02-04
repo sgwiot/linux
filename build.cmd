bef42c1eaedac2e0c2d5cc2a4e05969b657a7911
TonyHo <373466062@qq.com>
2021-02-04 16:36:54 +0800

    feat: add UUID driver, not working

--- build.cmd
+++ build.cmd
@@ -11,4 +11,9 @@ sudo cp arch/arm/boot/dts/sgw.dtb arch/arm/boot/zImage /mnt/
 sudo cp -r ../target-kernel/lib/modules/5.4.42-g4107092-dirty/ /mnt/
 make zImage dtbs INSTALL_MOD_PATH=../target-kernel modules_install
 sudo cp arch/arm/boot/dts/imx6ul-ccimx6ulsbcpro.dtb /mnt/pro.dtb
+<<<<<<< HEAD
 sudo mount AndesIDE.local:/home/telink/tftpboot Test
+=======
+>>>>>>> Coding/master
+make modules -j7; mkdir -p  ../target-kernel-54; make zImage dtbs INSTALL_MOD_PATH=../target-kernel-54 modules_install -j7 
+make modules -j7; mkdir -p  ../target-kernel-54; make zImage dtbs INSTALL_MOD_PATH=../target-kernel-54 modules_install -j7 
